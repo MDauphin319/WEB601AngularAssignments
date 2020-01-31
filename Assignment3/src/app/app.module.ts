5d71f905daebbe0e52446386d1d29e3cf4423342
Matt Dauphin <matthew.dauphin01@stclairconnect.ca>
2020-01-31 18:03:11 -0500

    Completed assignment 3

---
+++ Assignment3/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { SafePipe } from './safe.pipe';
+import { ContentListComponent } from './content-list/content-list.component';
+import { FilterTypePipe } from './filter-type.pipe';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentCardComponent,
+    SafePipe,
+    ContentListComponent,
+    FilterTypePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Assignment3/src/app/content-list/content-list.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import {Content} from '../content-card/content-card-helper';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.css']
+})
+export class ContentListComponent implements OnInit {
+  bandList: Content[];
+  constructor() {
+    this.bandList = [];
+    this.bandList[0] = {
+      id: 0,
+      author: 'Derek',
+      imgUrl: 'https://media.altpress.com/uploads/2019/09/green-day-ap-cover.jpg',
+      type: 'Story',
+      title: 'Green Day',
+      body: "Green Day's big hit when they came back was American Idiot",
+    };
+    this.bandList[1] = {
+      id: 1,
+      title: 'Katy Perry',
+      author: 'Jack',
+      body: `Good thing she isn't married to Russell Brand anymore`,
+      type: 'News',
+      imgUrl: 'https://i.pinimg.com/474x/fd/b7/bf/fdb7bf03d038de42649cf72af76a51bb--katy-perry-albums-cd-artwork.jpg',
+    };
+    this.bandList[2] = {
+      id: 2,
+      title: 'Metallica',
+      author: 'Stephen',
+      body: `They\'ve been making music since the beginning of time and it\'s great!`,
+      type: 'Story',
+      imgUrl: 'https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/ridethelightning.jpg?itok=Fd0KtaS2&timestamp=1549044407',
+      tags: ['classic', 'guitar hero']
+    };
+    this.bandList[3] = {
+      id: 3,
+      title: 'Wu Tang Clan',
+      author: 'Jd',
+      body: 'They were popular when I was in gradeschool',
+      type: 'News',
+      imgUrl: 'https://upload.wikimedia.org/wikipedia/en/5/53/Wu-TangClanEntertheWu-Tangalbumcover.jpg',
+    };
+    this.bandList[4] = {
+      id: 4,
+      title: 'Skrillex',
+      author: 'Stephen',
+      body: 'He used to be the lead singer of From First to Last',
+      type: 'Story',
+      imgUrl: 'https://www.billboard.com/files/styles/1536x866/public/media/Skrillex-Scary-Monsters-Nice-Sprites-2019-billboard-1500.jpg',
+      tags: ['guitar hero']
+    };
+
+  }
+
+  ngOnInit() {
+  }
+
+  CheckTitle(title:string) {
+    let isExist: boolean = false;
+    for (let i of this.bandList) {
+      if(i.title === title){ isExist = true; }
+    }
+    if(isExist)confirm('Found');
+    else { confirm('Not Found'); }
+  }
+}
